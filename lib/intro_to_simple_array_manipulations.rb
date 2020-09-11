b6a12fa2db43f27b8bec849829659b1cd8b174c7
Sharad Satsangi <s.satsangi@gmail.com>
2020-09-11 23:26:11 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,26 @@
+def using_concat (array1, array2)
+  array1.concat(array2)
+end
+
+
+def using_insert (array, element, index=4)
+  array.insert(index, element)
+end
+
+
+def using_uniq array
+  array.uniq
+end
+
+
+def using_flatten
+  array.flatten.to_s
+end
+
+
+def using_delete
+end
+
+
+def using_delete_at
+end
